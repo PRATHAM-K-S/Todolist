9f5c40d6057a41c6375b441d60cbae81fd1e7344
Pratham K S <prathamks2302@gmail.com>
2024-06-06 18:42:04 +0530

    added status toggle option in Todo component

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -14,6 +14,7 @@ const Todo = ({ title, description, id }) => {
   const [error, setError] = useState("");
   const [deleteLoading, setDeleteLoading] = useState(false);
   const [saveLoading, setSaveLoading] = useState(false);
+  const [completed, setCompleted] = useState(false);
 
   const handleEdit = () => {
     setSaveBtn((prev) => !prev);
@@ -54,59 +55,91 @@ const Todo = ({ title, description, id }) => {
     }
   };
 
+  const handleCompleted = async () => {
+    setCompleted(!completed);
+    try {
+      const result = await appwriteDataService.updateTodos(
+        titleRef.current.id,
+        { status: !completed }
+      );
+      if (result) {
+        console.log(result);
+        dispatch(increment());
+      }
+    } catch (error) {
+      setError(error);
+    }
+  };
+
   return (
-    <div className="w-full bg-black text-white h-[9rem] px-4 rounded-md my-2">
-      <div className="absolute flex flex-col">
-        <input
-          value={todoTitle}
-          className="text-xl font-semibold mt-4 bg-transparent outline-none"
-          ref={titleRef}
-          readOnly
-          id={id}
-          placeholder="Todo title"
-          onChange={(e) => setTodoTitle(e.target.value)}
-        />
-        <input
-          value={todoDescription}
-          ref={descRef}
-          placeholder="Description"
-          onChange={(e) => setTodoDescription(e.target.value)}
-          className="text-xs mt-2 font-light outline-none bg-transparent"
-          readOnly
-        />
-      </div>
-      <div className="flex gap-1 relative top-24">
-        {!saveBtn && (
+    <div className="w-full bg-black text-white h-[9rem] px-4 rounded-md my-2 flex justify-between items-start">
+      <div className="w-3/5">
+        <div className="flex flex-col">
+          <input
+            value={todoTitle}
+            className="text-xl font-semibold mt-4 bg-transparent outline-none"
+            ref={titleRef}
+            readOnly
+            id={id}
+            placeholder="Todo title"
+            onChange={(e) => setTodoTitle(e.target.value)}
+          />
+          <input
+            value={todoDescription}
+            ref={descRef}
+            placeholder="Description"
+            onChange={(e) => setTodoDescription(e.target.value)}
+            className="text-xs mt-2 font-light outline-none bg-transparent"
+            readOnly
+          />
+        </div>
+        <div className="flex gap-1 mt-8">
+          {!saveBtn && (
+            <button
+              type="button"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-sm text-xs px-5 py-1 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              onClick={handleEdit}
+            >
+              Edit
+            </button>
+          )}
+          {saveBtn && (
+            <button
+              type="button"
+              className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-sm text-xs px-5 py-1 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 flex gap-2 items-center"
+              onClick={handleSave}
+            >
+              Save
+              {saveLoading && (
+                <Loader width="w-3" height="h-3" fillColor="fill-white" />
+              )}
+            </button>
+          )}
           <button
             type="button"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-sm text-xs px-5 py-1 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-            onClick={handleEdit}
+            className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-sm text-xs px-5 py-1 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 flex gap-2 items-center"
+            onClick={handleDelete}
           >
-            Edit
-          </button>
-        )}
-        {saveBtn && (
-          <button
-            type="button"
-            className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-sm text-xs px-5 py-1 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 flex gap-2 items-center"
-            onClick={handleSave}
-          >
-            Save
-            {saveLoading && (
+            Delete{" "}
+            {deleteLoading && (
               <Loader width="w-3" height="h-3" fillColor="fill-white" />
             )}
           </button>
-        )}
-        <button
-          type="button"
-          className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-sm text-xs px-5 py-1 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 flex gap-2 items-center"
-          onClick={handleDelete}
-        >
-          Delete{" "}
-          {deleteLoading && (
-            <Loader width="w-3" height="h-3" fillColor="fill-white" />
+        </div>
+      </div>
+      <div className="flex flex-col text-xs gap-16 items-end mt-4">
+        <input
+          onChange={handleCompleted}
+          type="checkbox"
+          className="accent-green-500 appearance-auto mt-2"
+        />
+        <p className="mt-1">
+          {completed ? (
+            <span className="text-emerald-500">Completed</span>
+          ) : (
+            <span className="text-orange-300">Not completed</span>
           )}
-        </button>
+        </p>
       </div>
     </div>
   );
